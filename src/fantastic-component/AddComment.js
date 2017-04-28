55552ba6f9b3bfc77ebdf90cea68ec44e6522053
Asbjørn Bakke <asbbak@knowit.no>
2017-04-28 14:13:29 +0200

    styling

--- src/fantastic-component/AddComment.js
+++ src/fantastic-component/AddComment.js
@@ -13,7 +13,12 @@ export default class AddComment extends Component {
         return(
            <form>
                <div style={{"flexDirection":"column"}}>
-                    <textarea rows="4" cols="50" style={{"display":"block", "marginBottom":"0.5rem"}}/>
+                    <textarea rows="4" style={{ "display":"block", 
+                        "marginBottom":"0.5rem", 
+                        "borderRadius":"12px", 
+                        "fontSize": "1.4rem",
+                        "padding": "0.7rem",
+                        "minWidth": "385px" }} />
                     <button type="submit" style={this.buttonStyle}>Legg til kommentar</button>
                </div>
            </form>
--- src/fantastic-component/Comment.js
+++ src/fantastic-component/Comment.js
@@ -4,7 +4,10 @@ export default class Comment extends Component {
     render(){
         return (
             <div style={{"marginBottom":"1rem", "flexDirection":"column"}}>
-                <div style={{"color":"#8E7FAE"}}>{this.props.comment.author}</div>
+                <div style={{"flexDirection":"row"}}>
+                    <span style={{"color":"#8E7FAE"}}>{this.props.comment.author}</span>
+                    <span style={{"color":"#75787B", "fontSize": "0.75rem", "marginLeft": "10px"}}>{this.props.comment.timestamp}</span>
+                </div>
                 <div style={{"color":"#75787B", "fontSize":"1.4rem"}}>{this.props.comment.content}</div>
             </div>
         )
